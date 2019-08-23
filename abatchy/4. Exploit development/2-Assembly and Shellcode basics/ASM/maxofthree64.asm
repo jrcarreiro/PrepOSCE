843694b10811f32edca90cfdebeeb4169b2a21cf
Arm One <junior@reversing.team>
2019-08-23 08:09:05 -0300

    ASM64 it´s OK, but  ASM32 fail

---
+++ abatchy/4. Exploit development/2-Assembly and Shellcode basics/ASM/maxofthree64.asm
@@ -0,0 +1,9 @@
+        global  maxofthree
+        section .text
+maxofthree:
+        mov     rax, rdi                ; result (rax) initially holds x
+        cmp     rax, rsi                ; is x less than y?
+        cmovl   rax, rsi                ; if so, set result to y
+        cmp     rax, rdx                ; is max(x,y) less than z?
+        cmovl   rax, rdx                ; if so, set result to z
+        ret                             ; the max will be in rax
